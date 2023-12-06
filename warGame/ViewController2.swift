f33f4915726ac38872507140e58c10af17eb3402
Mason Bartell <masonjbartell@gmail.com>
2023-12-06 09:46:29 -0600

    initial

---
+++ warGame/ViewController2.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController2.swift
+//  warGame
+//
+//  Created by MASON BARTELL on 12/6/23.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
