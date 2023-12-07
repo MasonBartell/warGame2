c1d3406ab5a38f39b51c913c979e975b8706fb52
Mason Bartell <masonjbartell@gmail.com>
2023-12-07 09:41:54 -0600

    updates

--- warGame/ViewController2.swift
+++ warGame/ViewController2.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
 
+
+
+
+
+class ViewController2: UIViewController {
+    @IBOutlet weak var player1Label: UILabel!
+    
+    @IBOutlet weak var ComLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
+    @IBAction func revealButton(_ sender: UIButton) {
+        var number1 = Int.random(in: 1...10)
+        var number2 = Int.random(in: 1...10)
+        player1Label.text = "\(number1)"
+        ComLabel.text = "\(number2)"
+        if(number1 > number2)
+        {
+            view.backgroundColor = UIColor.green
+        }
+        else
+        {
+            view.backgroundColor = UIColor.red
+        }
+        
+    }
+    
     
 
 }
